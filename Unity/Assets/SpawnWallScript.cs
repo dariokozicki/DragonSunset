c1bb1cca83881bd7b88a2626a4a9872982f6a099
zdextiny <agustincavalie14@gmail.com>
2022-01-30 05:06:20 -0300

    actualicaciones de objetos

--- Unity/Assets/SpawnWallScript.cs
+++ Unity/Assets/SpawnWallScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpawnWallScript : MonoBehaviour
 {
-    [SerializeField]GameObject enemyWall;
+    [SerializeField] GameObject enemyWall;
     GameObject prefab;
     public float rotacionActual;
     void Start()
