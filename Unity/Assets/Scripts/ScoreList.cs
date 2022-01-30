97db1e057d6f540b9ba287f3cd7dfa8a0e9ac3d5
Darío <dariokozicki@gmail.com>
2022-01-29 22:51:58 -0300

    connect scoreboard

--- Unity/Assets/Scripts/ScoreList.cs
+++ Unity/Assets/Scripts/ScoreList.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class ScoreList : MonoBehaviour
 {
+    ContractInfo contractInfo;
+    public UnityEngine.UI.VerticalLayoutGroup verticalLayoutGroup ;
+
     // Start is called before the first frame update
-    void Start()
+    public List<PlayerScore> scoreboard;
+    async void Start()
     {
-        
+        contractInfo = GameObject.FindGameObjectWithTag("tagContractInfo").GetComponent<ContractInfo>();
+        scoreboard = await contractInfo.getScoreboard();
+        foreach (PlayerScore score in scoreboard) {
+            print(score.name);
+        }
     }
 
     // Update is called once per frame
