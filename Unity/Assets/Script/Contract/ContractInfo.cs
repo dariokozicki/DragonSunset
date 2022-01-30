082f7a264fa268bd6b3fe6e813db5719a431eef7
Darío <dariokozicki@gmail.com>
2022-01-30 10:11:33 -0300

    add functionality to get best score of current user

--- Unity/Assets/Script/Contract/ContractInfo.cs
+++ Unity/Assets/Script/Contract/ContractInfo.cs
@@ -11,6 +11,7 @@ public class ContractInfo : MonoBehaviour
     public static string METHOD_SCOREBOARD = "scoreboard";
     public static string METHOD_PLAYERSCORE = "hiScores";
     public static string METHOD_SETSCORE = "setScore";
+    public static string METHOD_CURRENTPLAYER_SCORE = "playerToScore";
     public static string abi = "[{\"anonymous\":false,\"inputs\":[{\"indexed\":false,\"internalType\":\"address\",\"name\":\"player\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"value\",\"type\":\"uint256\"},{\"indexed\":false,\"internalType\":\"string\",\"name\":\"name\",\"type\":\"string\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"timestamp\",\"type\":\"uint256\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"durationMillis\",\"type\":\"uint256\"}],\"name\":\"NewHiScore\",\"type\":\"event\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"name\":\"hiScores\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"name\",\"type\":\"string\"},{\"internalType\":\"uint256\",\"name\":\"value\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"timestamp\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"durationMillis\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"name\":\"playerToScore\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"scoreSize\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"scoreboard\",\"outputs\":[{\"internalType\":\"uint256[]\",\"name\":\"\",\"type\":\"uint256[]\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"_value\",\"type\":\"uint256\"},{\"internalType\":\"string\",\"name\":\"_name\",\"type\":\"string\"},{\"internalType\":\"uint256\",\"name\":\"_durationMillis\",\"type\":\"uint256\"}],\"name\":\"setScore\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]";
     public static string contract = "0x6AE4598d853d11C1984f520A6dD67AdEf2328888";
     // https://chainlist.org/
@@ -52,11 +53,13 @@ public class ContractInfo : MonoBehaviour
     }
 
     public async Task<int> setScore(long value, string name, int durationMillis) {
-        // array of arguments for contract
+        long previousScore = await getCurrentPlayerScore();
+        if (previousScore >= value){
+            throw new System.Exception("User didn't make a personal High Score!" +
+             "Previous: " + previousScore + ", Current: " + value);
+        }
         // value in wei
         string eth = "0";
-        // account to sent tokens to
-        string toAccount = PlayerPrefs.GetString("Account");
         string[] obj = { ""+value,""+ name,""+ durationMillis };
         string args = JsonConvert.SerializeObject(obj);
         // create data to interact with smart contract
@@ -69,6 +72,20 @@ public class ContractInfo : MonoBehaviour
         string response = await Web3Wallet.SendTransaction(chainId, contract, eth, data, gasLimit, gasPrice);
         print(response);
         return 1;
+    }
+
+    public async Task<int> getCurrentPlayerScoreId() {
+        string account = PlayerPrefs.GetString("Account");
+        if (string.IsNullOrEmpty(account)) { throw new System.Exception("User didn't load their wallet"); }
+        string args = "[\""+account+"\"]";
+        string response = await EVM.Call(chain, network, contract, abi, METHOD_CURRENTPLAYER_SCORE, args);
+        return int.Parse(response);
+    }
+
+    public async Task<long> getCurrentPlayerScore() {
+        int scoreId = await getCurrentPlayerScoreId();
+        PlayerScore score = await getPlayerScore(scoreId);
+        return score.value;
     }
 
     // Update is called once per frame
