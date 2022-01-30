569f43435fb61cf69afe43c43e74411a1ca94dfc
Darío <dariokozicki@gmail.com>
2022-01-30 07:35:16 -0300

    add styling to leaderboard

--- Unity/Assets/Script/Contract/ScoreList.cs
+++ Unity/Assets/Script/Contract/ScoreList.cs
@@ -12,9 +12,13 @@ public class ScoreList : MonoBehaviour
     public int nameFontSize;
     public float scoreHeight;
     public float scoreWidth;
+    public float nameWidth;
+    public float otherTextWidth;
+    public Color nameColor;
 
     async void Start()
     {
+        nameColor = new Color(62f/255.0f,21f/255.0f,21f/255.0f);
         contractInfo = GameObject.FindGameObjectWithTag("tagContractInfo").GetComponent<ContractInfo>();
         scoreboard = await contractInfo.getScoreboard();
         for (int i = 0; i < scoreboard.Count; i++) {
@@ -26,7 +30,7 @@ public class ScoreList : MonoBehaviour
         RectTransform boardRt = scoreContainer.GetComponent<RectTransform>();
         boardRt.sizeDelta = new Vector2(
             boardRt.sizeDelta.x, 
-            Mathf.Max(((RectTransform)transform).rect.height, scoreboard.Count * scoreHeight * 2f)
+            Mathf.Max(((RectTransform)transform).rect.height, scoreboard.Count * scoreHeight)
         );
     }
 
@@ -45,21 +49,37 @@ public class ScoreList : MonoBehaviour
         displayText(theScore, playerScore.name, nameFontSize ,"name", index, 0);
         displayText(theScore, "POINTS: " + playerScore.value, nameFontSize, "points", index, 1);
         displayText(theScore, "TIME: " + playerScore.durationMillis + " ms", nameFontSize, "time", index, 2);
-        displayText(theScore, "" + playerScore.timestamp, nameFontSize, "date", index, 3);
+        displayText(theScore, formatDate(playerScore.timestamp), nameFontSize, "date", index, 3);
     }
 
     void displayText(GameObject theScore, string text, int fontSize, string objName, int index, int paramIndex) { 
-        int textWidth = paramIndex == 0 ? 600 : 400;
-        int leftOffset = paramIndex == 0 ? 0 : 100;
         GameObject obj = new GameObject(objName, typeof(RectTransform));
         obj.transform.SetParent(theScore.transform);
         RectTransform rectObj = obj.GetComponent<RectTransform>();
-        rectObj.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, textWidth*paramIndex+leftOffset, textWidth);
-        rectObj.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, scoreHeight*index, scoreHeight);
+        rectObj.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 
+            nameWidth*Mathf.Min(paramIndex, 1) + otherTextWidth*Mathf.Max(paramIndex-1, 0), 
+            paramIndex == 0 || paramIndex == 3 ? nameWidth : otherTextWidth 
+        );
+        rectObj.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 0, scoreHeight);
         Text name = obj.AddComponent<Text>();
         name.text = text;
         name.font = nameFont;
-        name.fontSize = paramIndex == 0 ? fontSize : 40;
+        name.fontSize = paramIndex == 0 ? fontSize : 50;
+        if(paramIndex == 0) {
+            name.color = nameColor;
+        }
     }
 
+    public static System.DateTime UnixTimeStampToDateTime( double unixTimeStamp )
+    {
+        // Unix timestamp is seconds past epoch
+        System.DateTime dtDateTime = new System.DateTime(1970,1,1,0,0,0,0,System.DateTimeKind.Utc);
+        dtDateTime = dtDateTime.AddSeconds( unixTimeStamp ).ToLocalTime();
+        return dtDateTime;
+    }
+
+    public string formatDate(int timestamp) {
+        System.DateTime dt = UnixTimeStampToDateTime(timestamp);
+        return dt.ToString();
+    }
 }
