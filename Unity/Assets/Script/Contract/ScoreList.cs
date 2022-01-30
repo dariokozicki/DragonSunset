4bf7e51b45fc2a2770104340ee4ecb7a4010da70
Darío <dariokozicki@gmail.com>
2022-01-30 04:58:34 -0300

    work in progress scorecontainer

--- Unity/Assets/Script/Contract/ScoreList.cs
+++ Unity/Assets/Script/Contract/ScoreList.cs
@@ -17,10 +17,13 @@ public class ScoreList : MonoBehaviour
     {
         contractInfo = GameObject.FindGameObjectWithTag("tagContractInfo").GetComponent<ContractInfo>();
         scoreboard = await contractInfo.getScoreboard();
-        foreach (PlayerScore score in scoreboard) {
+        for (int i = 0; i < scoreboard.Count; i++) {
+            PlayerScore score = scoreboard[i];
             print(score.name);
-            displayScore(score);
+            displayScore(score, i);
         }
+        RectTransform boardRt = scoreContainer.GetComponent<RectTransform>();
+        boardRt.sizeDelta = new Vector2(boardRt.sizeDelta.x, scoreboard.Count * scoreHeight);
     }
 
     // Update is called once per frame
@@ -29,14 +32,14 @@ public class ScoreList : MonoBehaviour
         
     }
 
-    void displayScore(PlayerScore playerScore) {
+    void displayScore(PlayerScore playerScore, int index) {
         GameObject theScore = new GameObject(playerScore.name + "'s Score", typeof(RectTransform));
         theScore.transform.SetParent(scoreContainer.transform);
         RectTransform rectScore = theScore.GetComponent<RectTransform>();
-        rectScore.offsetMin = new Vector2(0,0);
-        rectScore.sizeDelta = new Vector2(scoreWidth, scoreHeight);
+        rectScore.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 20, scoreWidth);
+        rectScore.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, scoreHeight*index, scoreHeight);
         Text name = theScore.AddComponent<Text>();
-        name.text = playerScore.name;
+        name.text = playerScore.name + "\n" + "Points: " + playerScore.value + "\n" + playerScore.timestamp + "\n" + playerScore.durationMillis;
         name.font = nameFont;
         name.fontSize = nameFontSize;
     }
